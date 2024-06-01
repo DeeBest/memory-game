66c9b8961b5e5bfa10934eb70f2b36a86e65ef8c
DeeBest <simphiwedladla8@gmail.com>
2024-06-01 15:19:49 +0200

    created structural components and applied basic styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import FooterIcon from './FooterIcon';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="contact-icons">
+        <FooterIcon
+          link="mailto:simphiwedladla8@gmail.com"
+          imgPath="/icons/gmail.png"
+          imgAltText="gmail-icon"
+          imgClass="contact-icon"
+        />
+        <FooterIcon
+          link="https://www.linkedin.com/in/dladla-simphiwe-89061a20a/"
+          imgPath="/icons/linkedin.png"
+          imgAltText="linkedin-icon"
+          imgClass="contact-icon"
+        />
+        <FooterIcon
+          link="https://github.com/DeeBest"
+          imgPath="/icons/github.webp"
+          imgAltText="github-icon"
+          imgClass="contact-icon"
+        />
+      </div>
+      <small>Created By &copy;Simphiwe {currentYear}.</small>
+    </footer>
+  );
+};
+export default Footer;
